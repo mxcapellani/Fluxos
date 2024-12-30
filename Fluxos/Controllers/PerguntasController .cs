2625071001445ab9abc930fe6e835d5fbe824acc
Maximiliano Carvalho <maximiliano.carvalho@portaldecompraspublicas.com.br>
2024-12-29 21:54:26 -0300

    inclusões do helis recebendo do node

--- Fluxos/Controllers/PerguntasController .cs
+++ Fluxos/Controllers/PerguntasController .cs
@@ -18,17 +18,47 @@ namespace Fluxos.Controllers
         }
 
         [HttpPost("inicio")]
-        public async Task<IActionResult> GetInicio([FromBody] dynamic model)
+        public async Task<IActionResult> GetInicio([FromBody] JsonElement model)
         {
-
-
-            var pergunta = await _service.GetPerguntaAsync("1");
-            if (pergunta == null)
+            try
             {
-                return NotFound(new { message = "Pergunta inicial não encontrada." });
+                if (model.TryGetProperty("data", out JsonElement dataElement) &&
+                    dataElement.TryGetProperty("message", out JsonElement messageElement) &&
+                    messageElement.TryGetProperty("_data", out JsonElement dataContentElement) &&
+                    dataContentElement.TryGetProperty("body", out JsonElement bodyElement))
+                {
+                    var mensagem = bodyElement.GetString();
+
+                    var pergunta = await _service.GetPerguntaAsync("1");
+                    if (pergunta == null)
+                    {
+                        return NotFound(new { message = "Pergunta inicial não encontrada." });
+                    }
+                    return Ok(new { pergunta, mensagem });
+                }
+                else
+                {
+                    return BadRequest(new { message = "Formato de entrada inválido ou campo 'body' não encontrado." });
+                }
             }
-            return Ok(pergunta);
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { message = "Erro ao processar o JSON.", error = ex.Message });
+            }
         }
+
+        //[HttpPost("inicio")]
+        //public async Task<IActionResult> GetInicio([FromBody] dynamic model)
+        //{
+
+
+        //    var pergunta = await _service.GetPerguntaAsync("1");
+        //    if (pergunta == null)
+        //    {
+        //        return NotFound(new { message = "Pergunta inicial não encontrada." });
+        //    }
+        //    return Ok(pergunta);
+        //}
 
         [HttpPost("start")]
         public async Task<IActionResult> Start([FromBody] ReceberMensagem message)
